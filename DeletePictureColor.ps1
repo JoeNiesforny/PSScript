8ce1195d47d73e6096519ab7ffb8f353fc1fb747
Joe <jozwludz@gmail.com>
2016-06-08 00:00:54 +0200

    refactor

--- DeletePictureColor.ps1
+++ DeletePictureColor.ps1
@@ -6,14 +6,41 @@ if ($Path.Length -lt 0)
     end
 }
 
-$picture = Get-Content $Path -ReadCount 0 -Encoding Ascii 
-$pictureCharArray = $picture.ToCharArray()
-
-$offset = 0
-for($i=10; $i -lt 14; $i++)
+function GetArrayFromPicture ($path)
 {
-$offset += [System.Convert]::ToInt32($pictureCharArray[$i])*[Math]::Pow(2,$i - 10)
+    $picture = Get-Content $Path -ReadCount 0 -Encoding Ascii 
+    $pictureCharArray = $picture.ToCharArray()
+    return $pictureCharArray
 }
+
+function ClearBitsFromArray ($offset, $array)
+{
+    for($r=$offset; $r -lt $array.Length; $r += 3)
+    {
+        $array[$r] = 0
+    }
+    return $array
+}
+
+function GetPixelOffsetFromHeader ($array)
+{
+    $offset = 0
+    for($i=10; $i -lt 14; $i++)
+    {
+        $offset += [System.Convert]::ToInt32($array[$i])*[Math]::Pow(2,$i - 10)
+    }
+    return $offset
+}
+
+function SaveNewPictureFromArray ($path, $array)
+{
+    $newpicture = -join $array
+    Set-Content $path -Value $newpicture -Encoding Ascii
+}
+
+$pictureCharArray = GetArrayFromPicture $Path
+
+$offset = GetPixelOffsetFromHeader $pictureCharArray
 
 Write-Host
 Write-Host ([System.String]::Format("Colors of picture begin from offset {0:X}", [System.Convert]::ToInt32($offset)))
@@ -22,26 +49,17 @@ $decision = Read-Host ("Please choose color which should be deleted (red, green,
 
 if ($decision -contains "blue")
 {
-    for($r=$offset; $r -lt $pictureCharArray.Length; $r += 3)
-    {
-        $pictureCharArray[$r] = 0
-    }
+    $pictureCharArray = ClearBitsFromArray $offset $pictureCharArray
 }
 if ($decision -contains "green")
 {
-    for($i=($offset+1); $i -lt $pictureCharArray.Length; $i += 3)
-    {
-        $pictureCharArray[$i] = 0
-    }
+    $offset += 1
+    $pictureCharArray = ClearBitsFromArray $offset $pictureCharArray
 }
 if ($decision -contains "red")
 {
-    for($i=($offset+2); $i -lt $pictureCharArray.Length; $i += 3)
-    {
-        $pictureCharArray[$i] = 0
-    }
+    $offset += 2
+    $pictureCharArray = ClearBitsFromArray $offset $pictureCharArray
 }
 
-$newpicture = -join $pictureCharArray
-$newPicturePath = $NewPath
-Set-Content $newPicturePath -Value $newpicture -Encoding Ascii
+SaveNewPictureFromArray $NewPath $pictureCharArray
